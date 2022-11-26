75d9da8296aaad2861c53453c4f7b1684c968095
Laura Lanna Brandão <laura.lannab@gmail.com>
2022-11-26 19:58:16 -0300

    Desafio finalizado

--- LogicaProgramacao/src/main/kotlin/exercicios/desafio2/ControleEstoque.kt
+++ LogicaProgramacao/src/main/kotlin/exercicios/desafio2/ControleEstoque.kt
@@ -1,91 +1,153 @@
 package exercicios.desafio2
 
 val menu = """
+    
+    #################################
     Digite a opção desejada no Menu:
     1..................Adicionar item
     2.....................Editar item
     3.........Exibir itens em estoque
     4...........Exibir todos os itens
     0...............Finalizar sistema
+    #################################
+    
 """.trimIndent()
 
-val estoque: MutableList<Item> = mutableListOf<Item>()
+class Item(var id: Int, var nome: String, var quantidade: Int)
 
-class Item(val id: Int, val nome: String, val quantidade: Int)
+val estoque: MutableList<Item> = mutableListOf<Item>()
 
 fun main() {
     controleEstoque()
 }
 
+fun controleEstoque() {
+    val finalizar = 0
+    println("\nBEM-VINDO AO CONTROLE DE ESTOQUE!")
+    menuPrincipal()
+
+}
+
+fun menuPrincipal() {
+    println(menu)
+    val opcao = readln().toInt()
+
+    when (opcao) {
+        1 -> adicionaProduto()
+        2 -> editaProduto()
+        3 -> exibeEstoque()
+        4 -> exibeTodos()
+        0 -> finalizar()
+        else -> Unit
+    }
+}
+
 fun adicionaProduto() {
     println("Digite o nome do produto:")
-    var nome = readln()
-    println("Digite a quantidade:")
-    var qtd = readln().toInt()
+    val nome = readln()
 
-    if (qtd > 999) throw LimiteEstoqueMaxException()
-    if (qtd < 0) throw LimiteEstoqueMinException()
-
-    estoque.add(Item(estoque.size, nome, qtd))
-
-    println("Produto adicionado com sucesso!")
+    try {
+        println("Digite a quantidade:")
+        val quantidade = readln().toInt()
+        validaQuantidade(quantidade)
+        estoque.add(Item(estoque.size, nome, quantidade))
+        println("Produto adicionado com sucesso!")
+    } catch (e: LimiteEstoqueException) {
+        println(e.localizedMessage)
+        menuPrincipal()
+    } catch (e: Exception) {
+        println("Valor inválido")
+        menuPrincipal()
+    }
+    menuPrincipal()
 }
 
 fun editaProduto() {
     if (estoque.isEmpty()) {
         println("Não há produtos no estoque para editar!")
     } else {
-        println("Menu Editar")
-        // TODO função exibir produtos aqui
-
         println("Informe o ID do produto que deseja editar:")
         val id = readln().toInt()
         if (id > estoque.size) {
             println("ID não encontrado")
         } else {
-
+            val idProduto = id - 1
+            println("Informe novo nome para o produto")
+            val novoNome = readln()
+            estoque[idProduto].nome = novoNome
+            println("Informe nova quantidade para o produto")
+            val novaQuantidade = readln().toInt()
+            estoque[idProduto].quantidade = novaQuantidade
+            println(
+                """
+                    
+                #####################################
+                Edição feita com sucesso!
+                
+                Novo nome: ${estoque[idProduto].nome}
+                Nova quantidade: ${estoque[idProduto].quantidade}
+                #####################################
+                
+            """.trimIndent()
+            )
         }
     }
+    menuPrincipal()
 }
 
-fun controleEstoque() {
-    println("Bem-vindo ao controle de estoque!")
-    do {
-        println(menu)
-
-        val categoria = readln().toInt()
-
-        when (categoria) {
-            1 -> {
-                adicionaProduto()
-            }
-
-            2 -> {
-                "editar"
-            }
-
-            3 -> {
-                "listar validos"
-            }
-
-            4 -> {
-                "listar todos"
-            }
-
-            else -> Unit
+fun exibeEstoque() {
+    println("Lista dos produtos em estoque:")
+    if (estoque.size != 0) {
+        estoque.forEach {
+            println(
+            """
+                
+            Nome: ${it.nome} Quantidade: ${it.quantidade}
+            
+        """.trimIndent()
+            )
         }
-    } while (categoria != 0)
+    } else {
+        println("Não há produtos a serem exibidos")
+    }
+    menuPrincipal()
 }
 
+fun exibeTodos() {
+    println("Lista de todos os produtos:")
+    if (estoque.size != 0) {
+        estoque.forEach {
+            println(
+                """
+                    
+            ID:#${"%04d".format(it.id)} Nome: ${it.nome} Quantidade: ${it.quantidade}
+            
+        """.trimIndent()
+            )
+        }
+    } else {
+        println("Não há produtos a serem exibidos")
+    }
+    menuPrincipal()
+}
 
-class LimiteEstoqueMaxException : Exception() {
-    override fun getLocalizedMessage(): String {
-        return "Erro: Quantidade maior que 999. (Quantidade Máxima = 999)"
+fun finalizar() {
+    println("Deseja mesmo sair do sistema? (S/N)")
+    when (readln().uppercase()) {
+        "S" -> return
+        "N" -> menuPrincipal()
     }
 }
 
-class LimiteEstoqueMinException : Exception() {
-    override fun getLocalizedMessage(): String {
-        return "Erro: Quantidade menor que 0. (Quantidade Mínima = 0)"
+fun validaQuantidade(qtd: Int) {
+    if (qtd > 999 || qtd < 0) {
+        throw LimiteEstoqueException()
     }
+}
+
+class LimiteEstoqueException : Exception() {
+    override fun getLocalizedMessage(): String {
+        return "Quantidade inválida! Quantidade mínima: 0; Quantidade máxima: 999"
+    }
+
 }
