e307ed3a491e23fb5a965cfd15f88049c1c38cdb
Laura Lanna Brandão <laura.lannab@gmail.com>
2023-02-13 18:21:58 -0300

    utroca de nome das variaveis

--- TecnicasProgramacao/untitled/src/main/kotlin/exercicioFormulario/ValidaCelular.kt
+++ TecnicasProgramacao/untitled/src/main/kotlin/exercicioFormulario/ValidaCelular.kt
@@ -1,9 +1,9 @@
 package exercicioFormulario
 
 fun validaCelular(input: String) {
-    val patternNumbersPhone = Regex("""^\([0-9]{2}\)\s9[1-9][0-9]{3}\-[0-9]{4}$""")
+    val regexCelular = Regex("""^\([0-9]{2}\)\s9[1-9][0-9]{3}\-[0-9]{4}$""")
 
-    return if(input.matches(patternNumbersPhone)){
+    return if(input.matches(regexCelular)){
         println("Celular válido!")
     } else{
         println("Celular inválido!")
--- TecnicasProgramacao/untitled/src/main/kotlin/exercicioFormulario/ValidaCpfCnpj.kt
+++ TecnicasProgramacao/untitled/src/main/kotlin/exercicioFormulario/ValidaCpfCnpj.kt
@@ -1,10 +1,10 @@
 package exercicioFormulario
 
 fun validaCpfCnpj(input: String) {
-    val patternNumbersCpfCnpj =
+    val regexCpfCnpj =
         Regex("""([0-9]{2}\.[0-9]{3}\.[0-9]{3}\/[0-9]{4}-[0-9]{2})|([0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2})""")
 
-    return if(input.matches(patternNumbersCpfCnpj)){
+    return if(input.matches(regexCpfCnpj)){
         println("CPF ou CNPJ válido!")
     } else{
         println("CPF ou CNPJ inválido!")
