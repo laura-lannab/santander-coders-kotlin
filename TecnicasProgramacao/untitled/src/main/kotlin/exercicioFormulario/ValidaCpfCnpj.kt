1ee61b6421335161bcb77ebcf730e9ddc624c30f
Laura Lanna Brandão <laura.lannab@gmail.com>
2023-02-13 18:31:34 -0300

    novo update em boas práticas

--- TecnicasProgramacao/untitled/src/main/kotlin/exercicioFormulario/ValidaCpfCnpj.kt
+++ TecnicasProgramacao/untitled/src/main/kotlin/exercicioFormulario/ValidaCpfCnpj.kt
@@ -4,9 +4,9 @@ fun validaCpfCnpj(input: String) {
     val regexCpfCnpj =
         Regex("""([0-9]{2}\.[0-9]{3}\.[0-9]{3}\/[0-9]{4}-[0-9]{2})|([0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2})""")
 
-    return if(input.matches(regexCpfCnpj)){
+    return if (input.matches(regexCpfCnpj)) {
         println("CPF ou CNPJ válido!")
-    } else{
+    } else {
         println("CPF ou CNPJ inválido!")
     }
 }
--- TecnicasProgramacao/untitled/src/main/kotlin/exercicioFormulario/ValidaEmail.kt
+++ TecnicasProgramacao/untitled/src/main/kotlin/exercicioFormulario/ValidaEmail.kt
@@ -3,9 +3,9 @@ package exercicioFormulario
 fun validaEmail(input: String) {
     val regexEmail = Regex("""^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$""")
 
-    return if(input.matches(regexEmail)){
+    return if (input.matches(regexEmail)) {
         println("Email válido!")
-    } else{
+    } else {
         println("Email inválido!")
     }
 }
